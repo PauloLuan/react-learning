7bb4ec0f64464a349ac5ea27a928b01d00c1f028
Paulo Luan <pauloluan.inova@gmail.com>
2019-11-15 06:30:53 -0300

    removed images and fixed index page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './src/todo-simple-store/App'
+import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
